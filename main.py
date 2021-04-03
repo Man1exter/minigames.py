5c5dc3032b4f7d05caf7dae52e809449efabecd0
Mariusz Perzynski <m.perzyk@interia.pl>
2021-04-03 14:09:29 +0200

    all elements with menu is done

--- main.py
+++ main.py
@@ -3,10 +3,21 @@ def main():
     print(" ")
     menu()
 
+
 def menu():
     print(" ")
     print("[1] PAPER/STONE/SCISSORS - GAME")
     print("[2] INFO ABOUT FILE/CATALOG / S")
     print("[3] RANDOM VALUE - GUESS WHAT")
+    print(" ")
+
+    wybor = int(input("POZYCJA Z MENU ======> "))
+
+    if wybor == 1:
+        print(" ")
+    elif wybor == 2:
+        print(" ")
+    elif wybor == 3:
+        print(" ")
 
 main()
