46332f278dcde7a1488bdc004e1b08a16fcc96c5
Mariusz Perzynski <m.perzyk@interia.pl>
2021-04-03 14:04:32 +0200

    3 posotion in menu

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 def main():
     print("Witaj w panelu => gier - aplikacji <=")
     print(" ")
+    menu()
+
+def menu():
     print(" ")
-
-
+    print("[1] PAPER/STONE/SCISSORS - GAME")
+    print("[2] INFO ABOUT FILE/CATALOG / S")
+    print("[3] RANDOM VALUE - GUESS WHAT")
 
 main()
