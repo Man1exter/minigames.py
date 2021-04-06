c8896025bc32cc933e5be4f318bf083995a374b1
Mariusz Perzynski <m.perzyk@interia.pl>
2021-04-06 13:44:20 +0200

    file with game connect to main file

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pygame
 import random
 import os
 from random import randint
+from pygart import start
 
 def main():
     print(" ")
@@ -126,39 +127,9 @@ def randomes():
 
 def snake():
       
-      print(" ")
-      print(" <=> SNAKE GAME <=> ")
-      print(" ")
-      
-pygame.init()
-window = pygame.display.set_mode((800, 600))
+    print(" ")
+    print(" <=> SNAKE GAME <=> ")
+    print(" ")
+    start()
 
-osx = 70
-osy = 50
-player = pygame.rect.Rect(osx, osy, 100, 100)  # tworzy prostokąt
-
-run = True
-while run:
-    pygame.time.Clock().tick(60)  # maksymalnie 60 fps
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:  # jeśli gracz zamknie okienko
-            run = False
-
-    keys = pygame.key.get_pressed()
-
-    speed = 5
-    if keys[pygame.K_RIGHT]:  # czy strzałka w prawo jest naciskana
-        osx += speed
-    if keys[pygame.K_LEFT]:  # strzałka w lewo
-        osx -= speed
-    if keys[pygame.K_UP]:  # strzałka w górę
-        osy -= speed
-    if keys[pygame.K_DOWN]:  # strzałka w dół
-        osy += speed
-
-    player = pygame.rect.Rect(osx, osy, 100, 100)
-
-    window.fill((24, 164, 240))  # rysowanie tła
-    pygame.draw.rect(window, (20, 200, 20), player)  # rysowanie gracza
-    pygame.display.update()
 main()
