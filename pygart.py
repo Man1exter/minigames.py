1a12f971a8529479a94b0e9f7216224e7b7e4afe
Mariusz Perzynski <m.perzyk@interia.pl>
2021-04-07 08:47:41 +0200

    update img/jpg

--- pygart.py
+++ pygart.py
@@ -1,49 +1,83 @@
 import pygame
+from random import randint
+
+pygame.init()
+window = pygame.display.set_mode((1024, 760)) # 1024 × 760 4:3
+
+class Player:
+    def __init__(self):
+        self.x_cord = 0  
+        self.y_cord = 0  
+        self.image = pygame.image.load("./jppp/rulon223223.png")  
+        self.width = self.image.get_width()  
+        self.height = self.image.get_height()  
+        self.speed = 4  
+        self.hitbox = pygame.Rect(self.x_cord, self.y_cord, self.width, self.height)
+
+    def tick(self, keys): 
+        if keys[pygame.K_w]:
+            self.y_cord -= self.speed
+        if keys[pygame.K_a]:
+            self.x_cord -= self.speed
+        if keys[pygame.K_s]:
+            self.y_cord += self.speed
+        if keys[pygame.K_d]:
+            self.x_cord += self.speed
+
+        self.hitbox = pygame.Rect(self.x_cord, self.y_cord, self.width, self.height)
+
+    def draw(self):
+        window.blit(self.image, (self.x_cord, self.y_cord))
+
+
+class Cash:
+    def __init__(self):
+        self.x_cord = randint(0, 1280)
+        self.y_cord = randint(0, 720)
+        self.image = pygame.image.load("banknot.png")
+        self.width = self.image.get_width()  
+        self.height = self.image.get_height()  
+        self.hitbox = pygame.Rect(self.x_cord, self.y_cord, self.width, self.height)
+
+    def tick(self):
+        self.hitbox = pygame.Rect(self.x_cord, self.y_cord, self.width, self.height)
+
+    def draw(self):
+        window.blit(self.image, (self.x_cord, self.y_cord))
 
 def start():
- pygame.init()
- window = pygame.display.set_mode((1024, 760)) # 1024×760 4:3
+    run = True
+    player = Player()
+    clock = 0
+    score = 0
+    banknotes = []
+    background = pygame.image.load("./jppp/ruloniktilt1.png")
+    while run:
+        clock += pygame.time.Clock().tick(60) / 1000  
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT: 
+                run = False
+        keys = pygame.key.get_pressed()
+        if clock >= 2:
+            clock = 0
+            banknotes.append(Cash())
 
- osx = 70
- osy = 50
- player = pygame.rect.Rect(osx, osy, 100, 100) 
+        player.tick(keys)
+        for banknote in banknotes:
+            banknote.tick()
 
- class Player:
-     def __init__(self):
-         self.osx = 0
-         self.osy = 0
-         self.width = 0
-         self.height = 0
-         self.avatar = pygame.image.load("/jppp/rulon223223.png")
+        for banknote in banknotes:
+            if player.hitbox.colliderect(banknote.hitbox):
+                banknotes.remove(banknote)
+                score += 1
 
-     def tick(self):
-        speed = 5
-        if keys[pygame.K_RIGHT]: 
-          osx += self.speed
-        if keys[pygame.K_LEFT]:  
-          osx -= self.speed
-        if keys[pygame.K_UP]:  
-          osy -= self.speed
-        if keys[pygame.K_DOWN]:  
-          osy += self.speed
+        window.blit(background, (0, 0))  
+        player.draw()
+        for banknote in banknotes:
+            banknote.draw()
+        pygame.display.update()
 
-     def draw(self):
-         window.blit(self.avatar,(self.osx,self.osy))
-
- run = True
- while run:
-    pygame.time.Clock().tick(60)  
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:  
-            run = False
-
-    keys = pygame.key.get_pressed()
-
-    player = pygame.rect.Rect(osx, osy, 100, 100)
-
-    window.fill((24, 164, 240))  # rysowanie tła
-    pygame.draw.rect(window, (20, 200, 20), player) 
-    pygame.display.update()
+    print(score)
 
 if __name__ == "__main__":
     start()
