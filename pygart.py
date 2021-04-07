f17fa7600726bcdc6eccecab16070d110ddb8509
Mariusz Perzynski <m.perzyk@interia.pl>
2021-04-07 08:37:50 +0200

    class Player

--- pygart.py
+++ pygart.py
@@ -6,31 +6,43 @@ def start():
 
  osx = 70
  osy = 50
- player = pygame.rect.Rect(osx, osy, 100, 100)  # tworzy prostokąt
+ player = pygame.rect.Rect(osx, osy, 100, 100) 
+
+ class Player:
+     def __init__(self):
+         self.osx = 0
+         self.osy = 0
+         self.width = 0
+         self.height = 0
+         self.avatar = pygame.image.load("/jppp/rulon223223.png")
+
+     def tick(self):
+        speed = 5
+        if keys[pygame.K_RIGHT]: 
+          osx += self.speed
+        if keys[pygame.K_LEFT]:  
+          osx -= self.speed
+        if keys[pygame.K_UP]:  
+          osy -= self.speed
+        if keys[pygame.K_DOWN]:  
+          osy += self.speed
+
+     def draw(self):
+         window.blit(self.avatar,(self.osx,self.osy))
 
  run = True
  while run:
-    pygame.time.Clock().tick(60)  # maksymalnie 60 fps
+    pygame.time.Clock().tick(60)  
     for event in pygame.event.get():
-        if event.type == pygame.QUIT:  # jeśli gracz zamknie okienko
+        if event.type == pygame.QUIT:  
             run = False
 
     keys = pygame.key.get_pressed()
 
-    speed = 5
-    if keys[pygame.K_RIGHT]:  # czy strzałka w prawo jest naciskana
-        osx += speed
-    if keys[pygame.K_LEFT]:  # strzałka w lewo
-        osx -= speed
-    if keys[pygame.K_UP]:  # strzałka w górę
-        osy -= speed
-    if keys[pygame.K_DOWN]:  # strzałka w dół
-        osy += speed
-
     player = pygame.rect.Rect(osx, osy, 100, 100)
 
     window.fill((24, 164, 240))  # rysowanie tła
-    pygame.draw.rect(window, (20, 200, 20), player)  # rysowanie gracza
+    pygame.draw.rect(window, (20, 200, 20), player) 
     pygame.display.update()
 
 if __name__ == "__main__":
